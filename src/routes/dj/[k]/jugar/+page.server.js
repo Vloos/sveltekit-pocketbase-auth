1cfdfdb46c3f8d1960db43f1ee2d3373e38aa0ed
Vloos <vlos@mundo-r.com>
2023-05-17 21:21:47 +0200

    Comprueba que el user es el dj

--- src/routes/dj/[k]/jugar/+page.server.js
+++ src/routes/dj/[k]/jugar/+page.server.js
@@ -1,6 +1,14 @@
 /** @type {import('./$types').PageLoad} */
-export async function load({locals}) {
+import { redirect } from '@sveltejs/kit';
+
+export async function load({params, locals}) {
   if (!locals.user) return { user: undefined }
+  // Comprobar la id del user. Comprobar la id del dueño de la partida. Si son distintos, no puede pasar
+  let campa = await locals.pb.collection('campana').getOne(params.k)
+
+  if (campa.dj !== locals.user.id){
+    throw redirect(300, '/' )
+  }
   
   return {user: locals.user}
 }
