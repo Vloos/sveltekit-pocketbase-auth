0b81a600ebf7fbd48126fecd379f3585692c9e10
Vloos <vlos@mundo-r.com>
2023-06-05 21:11:26 +0200

    clase y editor

---
+++ src/lib/components/Personaje/ficha.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Personaje from '$lib/pj/Personaje'
+  export let pj
+</script>
+
+
+
+<section>
+  <header>Características</header>
+  <ul>
+    {#each Personaje.nombreCar as car,i (i)}
+      <li>
+        <article>
+          <header>{car}</header>
+          <span>
+            <input 
+              type="number" 
+              min={Personaje.minMaxCarPuntos[0]} 
+              max={Personaje.maxMaxCarPuntos[1]} 
+              bind:value={pj.caracteristicas[i]}>
+            </span>
+        </article>
+      </li>
+    {/each}
+  </ul>
+</section>
+
+<section>
+  <header>Habilidades</header>
+  <ul>
+    {#each Personaje.nombreHabs as hab, i (i)}
+      <li>
+        <article>
+          <header>{hab} <span>{Personaje.nombreCar[Personaje.habMods[i]]}</span></header>
+          <span>
+            <input 
+              type="number"
+              min={Personaje.minManxHabPuntos[0]}
+              max={Personaje.maxManxHabPuntos[1]}
+              bind:value={pj.habilidadesNivel[i]}
+              >
+              +{pj.habDatos(i).carNivel}
+          </span>
+          <span>
+            {pj.habDatos(i).total}
+          </span>
+        </article>
+      </li>
+    {/each}
+  </ul>
+</section>
